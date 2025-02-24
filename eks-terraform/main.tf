bedec39d8de3a99a1c3e757769426483e067701b
holadmex <ojewumidimeji@gmail.com>
2025-02-24 19:23:40 +0100

    new changes

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -1,12 +1,27 @@
-resource "aws_eks_node_group" "eks_nodes" {
+resource "aws_eks_node_group" "eks_nodes_1" {
   cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "eks-node-group"
+  node_group_name = "eks-node-group-1"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.subnet_ids
   instance_types  = ["t3.medium"]
+  
   scaling_config {
     desired_size = 2
     max_size     = 3
+    min_size     = 1
+  }
+}
+
+resource "aws_eks_node_group" "eks_nodes_2" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "eks-node-group-2"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = var.subnet_ids
+  instance_types  = ["t3.small"]
+  
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
     min_size     = 1
   }
 }
